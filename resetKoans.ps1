f27c5dbcff62a8f824a969ab53bd24c7b1a28a69
Devlin Liles <dliles@eprod.com>
2014-06-24 13:01:05 -0500

    Adding reset file.

--- resetKoans.ps1
+++ resetKoans.ps1
@@ -1,7 +1,1 @@
-$files = Get-ChildItem .\koans -Recurse -Force -ErrorAction SilentlyContinue -Filter *.js| % `
-{
-    $filepath = $_.FullName;
-    (get-content $filepath) |
-        % { $_ -replace 'expect\((.+)\)','expect(toDo)' } |
-        set-content $filepath -Force
-}
+$files = Get-ChildItem .\koans -Recurse -Force -ErrorAction SilentlyContinue -Filter *.js| % { $filepath = $_.FullName; (get-content $filepath) | % { $_ -replace 'expect\((.+)\)\.','expect(toDo).' } | set-content $filepath -Force }
